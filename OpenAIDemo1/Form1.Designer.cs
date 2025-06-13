c955862baaf2256f4400d60138f93b8f2cbf2400
savelor@microsoft.com <savelor@microsoft.com>
2025-06-13 12:04:53 +0200

    Add project files.

---
+++ OpenAIDemo1/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿
+namespace OpenAIDemo1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 130);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1365, 1379);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // webView21
+            // 
+            this.webView21.AllowExternalDrop = true;
+            this.webView21.CreationProperties = null;
+            this.webView21.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView21.Location = new System.Drawing.Point(1452, 130);
+            this.webView21.Name = "webView21";
+            this.webView21.Size = new System.Drawing.Size(1195, 1276);
+            this.webView21.TabIndex = 1;
+            this.webView21.ZoomFactor = 1D;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1452, 1434);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(212, 75);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "REFACTOR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(316, 33);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Azure OpenAI endpoint";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(18, 64);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(536, 40);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(602, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 33);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "API key";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(601, 64);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(376, 40);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(1033, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(175, 33);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Model name";
+            // 
+            // textBox3
+            // 
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(1039, 64);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(163, 40);
+            this.textBox3.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(1446, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(167, 33);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "SQL Server";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(1452, 63);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(394, 40);
+            this.textBox4.TabIndex = 10;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(2412, 57);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(166, 50);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Connect";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(1883, 23);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(139, 33);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Database";
+            // 
+            // textBox5
+            // 
+            this.textBox5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox5.Location = new System.Drawing.Point(1889, 64);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(430, 40);
+            this.textBox5.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2686, 1531);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.webView21);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "SQLAI Refactoring tool";
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+    }
+}
+
---
+++ OpenAIDemo1/Form1.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenAI.Chat;
+using Azure.Core;
+using Azure.AI.OpenAI;
+using Azure;
+using System.IO;
+using System.Data.SqlClient;
+using System.Xml;
+
+namespace OpenAIDemo1
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection sqlConnection; // Connessione riutilizzabile
+        public static string SQLDataTypes = "";
+        public static string ExecutionPlan = "";
+        public static string ActualExecutionPlan = "";
+        public static string EstimatedExecutionPlan = "";
+        public static string IndexesList = "";
+        public static string model = "";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.Load += Form1_Load;
+
+
+            //------inizializziamo la grafica
+            this.Size = new Size(1740, 1110); // Allarga il form
+            this.StartPosition = FormStartPosition.CenterScreen; // Centra all'avvio
+
+            // Set RichTextBox1 (on the left)
+            richTextBox1.Location = new Point(10, 70);
+            richTextBox1.Size = new Size(880, 1010);
+            richTextBox1.Anchor = AnchorStyles.Top | AnchorStyles.Left;
+
+            // Set webView21 (on the right)
+            webView21.Location = new Point(895, 70);
+            webView21.Size = new Size(820, 900);
+            webView21.Anchor = AnchorStyles.Top | AnchorStyles.Left;
+
+            button1.Location = new Point(1580, 1010);
+            button1.Size = new Size(80, 40);
+            button1.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            button1.BringToFront();
+
+            this.MinimizeBox = true;       // Abilita il pulsante di minimizzazione
+            this.MaximizeBox = true;       // (Facoltativo) Abilita il pulsante di massimizzazione
+
+            this.textBox4.Text = "localhost\\sqldashboard";
+            this.textBox5.Text = "AdventureWorks2019";
+        }
+
+
+        //===============================Funzione separata che invia il messaggio e ottiene la risposta
+        private async Task<string> GetChatResponseSavelor(string chatContext,
+                                                            string SQLQueryToOptimize,
+                                                            string Training1,
+                                                            string Training2,
+                                                            string Training3)
+        {
+            var endpoint = new Uri("https://aisavelor.openai.azure.com/");
+            var apiKey = "d601113d574940538109ee59dde7527e";
+            var deploymentName = "";
+            ChatCompletionOptions requestOptions = null;
+
+            //------inizializzazione per 4o
+            deploymentName = "gpt-4o";
+            requestOptions = new ChatCompletionOptions()
+            {
+                MaxOutputTokenCount = 8192,
+                Temperature = 0.4f,
+                TopP = 1.0f
+            };
+
+            ////inizializziamo il client o3 - mini
+            //model = "o3-mini";
+            //deploymentName = "o3-mini";
+            //requestOptions = new ChatCompletionOptions()
+            //{
+            //    MaxOutputTokenCount = 100000
+            //};
+            //requestOptions.SetNewMaxCompletionTokensPropertyEnabled(true);
+            
+
+            //valido per tutti i modelli
+            var azureClient = new AzureOpenAIClient(endpoint, new AzureKeyCredential(apiKey));
+            var chatClient = azureClient.GetChatClient(deploymentName);
+
+            // Crea i messaggi per la chat
+            List<ChatMessage> messages = new List<ChatMessage>()
+            {
+                new SystemChatMessage(chatContext),
+                new UserChatMessage(SQLQueryToOptimize),
+                new UserChatMessage(Training1),
+                new UserChatMessage(Training2),
+                new UserChatMessage(Training3)
+            };
+
+            // Ottieni la risposta dall'assistente
+            var response = await chatClient.CompleteChatAsync(messages, requestOptions);
+            string risposta = response.Value.Content[0].Text;
+            risposta = risposta.Replace("\n", "\r\n");
+            return risposta;
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            Color inactiveBorder = SystemColors.InactiveBorder;
+            string coloreHtml = $"rgb({inactiveBorder.R}, {inactiveBorder.G}, {inactiveBorder.B})";
+
+            // Assicurati che WebView2 sia inizializzato
+            await webView21.EnsureCoreWebView2Async(null);
+            string html = $@"
+                    <html>
+                    <head>
+                        <style>
+                            body {{
+                                font-family: Arial;
+                                margin: 0;
+                                padding: 10px;
+                                height: 100%;
+                                overflow-y: scroll;
+                                font-size: 16px;
+                                background-color: {coloreHtml};
+                            }}
+                            #content {{
+                                display: flex;
+                                flex-direction: column;
+                            }}
+                        </style>
+                    </head>
+                    <body>
+                        <div id='content' style='margin-top:850px; font-size: 12px;'>
+                            <div style='font-size:24px'>Welcome to SQL assistant!</div>
+                        </div>
+                        <script>
+                            document.body.style.zoom = '70%';
+                            document.body.style.backgroundColor = '{coloreHtml}';
+                        </script>
+                    </body>
+                    </html>";
+
+            // Carica il contenuto iniziale
+            webView21.CoreWebView2.NavigateToString(html);
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+
+            string chatContext = "You are a SQL Server developer. Analyze and Rewrite the T-SQL batch that you have in input " +
+                "applying the best practice rules that are given to you. Then add your observations. All the content returned must be in HTML.";
+
+            //Load Query to Optimize
+            string SQLQueryToOptimize = "This is the query to analyze: " + Environment.NewLine + richTextBox1.Text + Environment.NewLine;  //Query to optimize
+
+            //Define the refactoring rules
+            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Training.txt");
+            string Training1 = File.ReadAllText(filePath);
+
+            //Load columns data types
+            string Training2 = "This is the list of all tables, columns and data types in JSON format:" + Environment.NewLine + SQLDataTypes + Environment.NewLine; 
+
+            string Training3 = "All the content returned must be in HTML. Put the optimized code on an Azure backgroud";
+
+            string risposta = await GetChatResponseSavelor(chatContext, SQLQueryToOptimize, Training1, Training2, Training3);
+
+            risposta = risposta.Replace("```html", "");
+            risposta = risposta.Replace("```", "");
+            string nuovoHtml = "<div>" + risposta + "</div>";
+            AggiungiContenutoHtml(nuovoHtml);
+        }  
+
+
+        private void AggiungiContenutoHtml(string nuovoHtml)
+        {
+            Color inactiveBorder = SystemColors.InactiveBorder;
+            string coloreHtml = $"rgb({inactiveBorder.R}, {inactiveBorder.G}, {inactiveBorder.B})";
+
+            string script = $@"
+            document.body.style.backgroundColor = '{coloreHtml}';
+            var contentDiv = document.getElementById('content');
+            var nuovoDiv = document.createElement('div');
+            nuovoDiv.innerHTML = `" + nuovoHtml.Replace("`", "\\`") + @"`;
+            contentDiv.appendChild(nuovoDiv);
+            document.body.style.zoom = '70%';
+
+            function smoothScrollToBottom(duration = 3000) {
+                const start = window.scrollY;
+                const end = document.body.scrollHeight;
+                const distance = end - start;
+                const startTime = performance.now();
+
+                function scrollStep(currentTime) {
+                    const elapsed = currentTime - startTime;
+                    const progress = Math.min(elapsed / duration, 1);
+                    window.scrollTo(0, start + distance * easeInOutQuad(progress));
+                    if (progress < 1) {
+                        requestAnimationFrame(scrollStep);
+                    }
+                }
+
+                function easeInOutQuad(t) {
+                    return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
+                }
+
+                requestAnimationFrame(scrollStep);
+            }
+
+            smoothScrollToBottom(3000); // ← cambia qui la durata in ms per più lentezza";
+
+            webView21.CoreWebView2.ExecuteScriptAsync(script);
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string serverName = textBox4.Text.Trim();
+            string databaseName = textBox5.Text.Trim();
+
+            if (string.IsNullOrEmpty(serverName) || string.IsNullOrEmpty(databaseName))
+            {
+                MessageBox.Show("Insert SQL Server name or Database Name");
+                return;
+            }
+
+            string connectionString = $"Server={serverName};Database={databaseName};Integrated Security=true;";
+            sqlConnection = new SqlConnection(connectionString);
+            try
+            {
+                sqlConnection.Open();
+                button2.ForeColor = Color.Green;
+                button2.Text = "Connected";
+                GetDataTypesInfo();  // Chiama qui la nuova funzione
+                sqlConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Connection Error: " + ex.Message);
+            }
+        }
+
+
+        public void GetDataTypesInfo()  //---Get all columns data types
+        {
+            try
+            {
+                string query = @" SELECT
+                                    SCHEMA_NAME(t.schema_id) AS [Schema],
+                                    t.name AS [Table],
+                                    c.name AS [Column],
+                                    CASE 
+                                        WHEN ty.name IN ('char', 'varchar', 'nchar', 'nvarchar', 'binary', 'varbinary') THEN 
+                                            ty.name + 
+                                            CASE 
+                                                WHEN c.max_length = -1 THEN '(MAX)'
+                                                ELSE '(' + CAST(c.max_length / 
+                                                    CASE 
+                                                        WHEN ty.name IN ('nchar', 'nvarchar') THEN 2 
+                                                        ELSE 1 
+                                                    END AS VARCHAR) + ')'
+                                            END
+                                        WHEN ty.name IN ('decimal', 'numeric') THEN 
+                                            ty.name + '(' + CAST(c.precision AS VARCHAR) + ',' + CAST(c.scale AS VARCHAR) + ')'
+                                        ELSE ty.name
+                                    END AS DataType
+                                FROM 
+                                    sys.tables t
+                                    INNER JOIN sys.columns c ON t.object_id = c.object_id
+                                    INNER JOIN sys.types ty ON c.user_type_id = ty.user_type_id
+                                ORDER BY 
+                                    t.name, c.column_id
+                                FOR JSON PATH ";
+
+                SqlCommand command = new SqlCommand(query, sqlConnection);
+                SqlDataReader reader = command.ExecuteReader();
+
+                string jsonResult = string.Empty;
+                if (reader.Read())
+                {
+                    jsonResult = reader.GetString(0); // Get JSON string from first column
+                }
+
+                reader.Close();
+                SQLDataTypes = jsonResult; // Save in global variable
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Errore durante l'esecuzione della query: " + ex.Message);
+            }
+        }
+
+    }
+}
